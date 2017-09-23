ec3878ebd55015e1acb0f6b37e3fbb2a9a0d8c70
Joshua Cleland <proxikal@gmail.com>
2017-09-23 12:37:44 -0400

    Delete GiphyAPI.js

--- Javascript/Functions/GiphyAPI.js
+++
@@ -1,9 +0,0 @@
-function GiphyAPI(apikey, keyword) {
-  var clean = Replace(keyword, " ", "-");
-  var obj = GetJSON("http://api.giphy.com/v1/gifs/random?api_key="+apikey+"&tag=" + clean);
-  if(obj.hasOwnProperty('data')) {
-    return obj["data"]["image_original_url"];
-  } else {
-    return "No results found";
-  }
-}
