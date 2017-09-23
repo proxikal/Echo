edf92c5a450fdcfb92df975a7c4b77fdd76303c8
Joshua Cleland <proxikal@gmail.com>
2017-09-22 23:53:13 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
+var original = Params;
 if(Params.indexOf(" ") >= 0) {
        Params = Replace(Params, " ", "-");
 }
 var obj = GetJSON("http://api.tenor.co/v1/search?tag=" + Params);
 if(obj.results.length > 0) {
-    resp = "Results for: `" + Params + "`:\n" + obj.results[0].url;
+    resp = "Results for: `" + original + "`:\n" + obj.results[0].url;
 } else {
     resp = "Sorry, no results found!";
 }
