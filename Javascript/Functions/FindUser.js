07ff34480d3e8e1537acd6b331024de24b489b5e
Joshua Cleland <proxikal@gmail.com>
2017-09-24 10:48:58 -0400

    Update FindUser.js

--- Javascript/Functions/FindUser.js
+++ Javascript/Functions/FindUser.js
@@ -1,6 +1,6 @@
 function FindUser(user) {
   for(i = 0; i < Server.Members.length; i++) {
-    if(user == Server.Members[i]) {
+    if(user == Server.Members[i].User.Username) {
       return true;
     }
   }
